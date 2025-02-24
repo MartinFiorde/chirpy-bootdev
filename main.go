1feb15a33de519a8b289fc591ebb068d8ade85c9
MartinFiorde <fiorde_90@hotmail.com>
2025-02-23 22:32:10 -0300

    feat: 1-8 deliver logo

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 
 func main() {
 	// run with "go build -o out && ./out" command in a new terminal to start server
+	// explorer path to index.html - http://localhost:8080/
+	// explorer path to assets/logo.png - http://localhost:8080/assets/logo.png
 	sv := http.NewServeMux()
 	svStruct := http.Server{
 		Addr: ":8080",
